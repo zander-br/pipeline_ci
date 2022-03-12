123d5d5270771fc824af45b7b4791bd16fbe8e88
Anderson Santos <51419725+zander-br@users.noreply.github.com>
2022-03-12 14:21:11 -0300

    feat: keep pipeline ci
    * feat: credo recommendations
    
    * feat: prepare coverage
    
    * chore: sobelow configs
    
    * fix: job sobelow name
    
    * fix: pipeline test
    
    * fix: pipeline test
    
    * chore: add dialyxir

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -16,6 +16,8 @@ defmodule PipelineCi.DataCase do
 
   use ExUnit.CaseTemplate
 
+  alias Ecto.Adapters.SQL.Sandbox
+
   using do
     quote do
       alias PipelineCi.Repo
@@ -28,8 +30,8 @@ defmodule PipelineCi.DataCase do
   end
 
   setup tags do
-    pid = Ecto.Adapters.SQL.Sandbox.start_owner!(PipelineCi.Repo, shared: not tags[:async])
-    on_exit(fn -> Ecto.Adapters.SQL.Sandbox.stop_owner(pid) end)
+    pid = Sandbox.start_owner!(PipelineCi.Repo, shared: not tags[:async])
+    on_exit(fn -> Sandbox.stop_owner(pid) end)
     :ok
   end
 
