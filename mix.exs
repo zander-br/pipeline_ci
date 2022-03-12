a16f2e6419f17014d628f1ae358b76d9c78172fb
Anderson Santos <anderson.feitoza.santos@gmail.com>
2022-03-12 14:08:52 -0300

    chore: add dialyxir

--- mix.exs
+++ mix.exs
@@ -58,7 +58,8 @@ defmodule PipelineCi.MixProject do
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.10", only: :test},
-      {:sobelow, "~> 0.8", only: :dev}
+      {:sobelow, "~> 0.8", only: :dev},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end
 
